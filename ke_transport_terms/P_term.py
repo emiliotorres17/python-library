59656db4bc64e2b30a99841fd654488ae4383d16
emiliotorres17 <emiliotorres17@yahoo.com>
2019-11-26 21:37:19 -0700

    finished unit testing the P term; pylint --> 10/10

--- ke_transport_terms/P_term.py
+++ ke_transport_terms/P_term.py
@@ -48,76 +48,178 @@ def p_term(
     #---------------------------------------------------------------------#
     # Calculating the terms in the P term                                 #
     #---------------------------------------------------------------------#
-    term1   = np.multiply(Tau11, S11)
-    term2   = 2.0*np.multiply(Tau12, S12)
-    term3   = 2.0*np.multiply(Tau13, S13)
-    term4   = np.multiply(Tau22, S22)
-    term5   = 2.0*np.multiply(Tau23, S23)
-    term6   = np.multiply(Tau33, S33)
+    Term1   = np.multiply(Tau11, S11)
+    Term2   = 2.0*np.multiply(Tau12, S12)
+    Term3   = 2.0*np.multiply(Tau13, S13)
+    Term4   = np.multiply(Tau22, S22)
+    Term5   = 2.0*np.multiply(Tau23, S23)
+    Term6   = np.multiply(Tau33, S33)
     #---------------------------------------------------------------------#
     # Calculating the P term                                              #
     #---------------------------------------------------------------------#
-    P   = term1 + term2 + term3 + term4 + term5 + term6
+    P   = Term1 + Term2 + Term3 + Term4 + Term5 + Term6
 
     return P
 #=========================================================================#
 # Main                                                                    #
 #=========================================================================#
 if __name__ == "__main__":
-    #---------------------------------------------------------------------#
+    #=====================================================================#
     # Main preamble                                                       #
-    #---------------------------------------------------------------------#
+    #=====================================================================#
     call(["clear"])
     sep         = os.sep
     pwd         = os.getcwd()
-    data_path   = pwd + "%cdata%c"              %(sep, sep)
+    media_path  = pwd + "%cmedia%c"              %(sep, sep)
     #---------------------------------------------------------------------#
     # Domain variables                                                    #
     #---------------------------------------------------------------------#
     pi          = np.pi
-    x1          = np.linspace(0.0, 2.0*pi, 64)
-    x2          = np.linspace(0.0, 2.0*pi, 64)
-    [X1, X2]    = np.meshgrid(x1, x2)
-    dx          = (2.0*pi)/64.0
+    N           = 350
+    x0          = 0.0
+    xf          = 1.0
+    x           = np.linspace(x0, xf, N+1)
+    y           = np.linspace(x0, xf, N+1)
+    z           = np.linspace(x0, xf, N+1)
+    [X1, X2]    = np.meshgrid(x, y)
+    dx          = (xf - x0)/N
+    #=====================================================================#
+    # Approximate solution                                                #
+    #=====================================================================#
     #---------------------------------------------------------------------#
-    # Loading data                                                        #
+    # Preallocating arrays                                                #
     #---------------------------------------------------------------------#
-    u1      = np.load(data_path + "velocity1.npy")
-    u2      = np.load(data_path + "velocity2.npy")
-    u3      = np.load(data_path + "velocity3.npy")
-    tau11   = np.load(data_path + "tau11.npy")
-    tau12   = np.load(data_path + "tau12.npy")
-    tau13   = np.load(data_path + "tau13.npy")
-    tau22   = np.load(data_path + "tau22.npy")
-    tau23   = np.load(data_path + "tau23.npy")
-    tau33   = np.load(data_path + "tau33.npy")
+    ux      = np.zeros((N+1, N+1, N+1))
+    uy      = np.zeros((N+1, N+1, N+1))
+    uz      = np.zeros((N+1, N+1, N+1))
+    tau11   = np.zeros((N+1, N+1, N+1))
+    tau12   = np.zeros((N+1, N+1, N+1))
+    tau13   = np.zeros((N+1, N+1, N+1))
+    tau22   = np.zeros((N+1, N+1, N+1))
+    tau23   = np.zeros((N+1, N+1, N+1))
+    tau33   = np.zeros((N+1, N+1, N+1))
     #---------------------------------------------------------------------#
-    # Defining specific time step data                                    #
+    # Calculating the velocities and the subgrid stresses                 #
     #---------------------------------------------------------------------#
-    tslice  = 25
-    u1      = u1[:,:,:,tslice]
-    u2      = u2[:,:,:,tslice]
-    u3      = u3[:,:,:,tslice]
-    tau11   = tau11[:,:,:,tslice]
-    tau12   = tau12[:,:,:,tslice]
-    tau13   = tau13[:,:,:,tslice]
-    tau22   = tau22[:,:,:,tslice]
-    tau23   = tau23[:,:,:,tslice]
-    tau33   = tau33[:,:,:,tslice]
+    print_count = 0
+    for k in range(0, N+1):
+        for j in range(0, N+1):
+            for i in range(0, N+1):
+                #---------------------------------------------------------#
+                # Velocities                                              #
+                #---------------------------------------------------------#
+                ux[i,j,k]       = x[i]**2.0*y[j]*z[k]
+                uy[i,j,k]       = x[i] + y[j] + z[k]
+                uz[i,j,k]       = 3.0*x[i] + 2.0*y[j] + z[k]
+                #---------------------------------------------------------#
+                # Subgrid stresses                                        #
+                #---------------------------------------------------------#
+                tau11[i,j,k]    = x[i]**2.0
+                tau12[i,j,k]    = x[i]*y[j]
+                tau13[i,j,k]    = x[i]*z[k]
+                tau22[i,j,k]    = y[j]**2.0
+                tau23[i,j,k]    = y[j]*z[k]
+                tau33[i,j,k]    = z[k]**2.0
+        #-----------------------------------------------------------------#
+        # Print statement                                                 #
+        #-----------------------------------------------------------------#
+        if print_count > 5:
+            print(k)
+            print_count = 0
+        print_count += 1
     #---------------------------------------------------------------------#
-    # Calculating the P term                                              #
+    # Calculating the approximate solution                                #
     #---------------------------------------------------------------------#
-    p   = p_term(u1, u2, u3, tau11, tau12, tau13, tau22, tau23, tau33, dx)
+    P_approx    = p_term(ux, uy, uz, tau11, tau12, tau13, tau22, tau23,\
+                            tau33, dx)
     #---------------------------------------------------------------------#
-    # Plotting P results                                                  #
+    # Deleting variables                                                  #
     #---------------------------------------------------------------------#
-    cnt = plt.contourf(X1, X2, p[:,:,32], 500, cmap="jet")
+    del ux
+    del uy
+    del uz
+    del tau11
+    del tau12
+    del tau13
+    del tau22
+    del tau23
+    del tau33
+    #=====================================================================#
+    # Exact solution                                                      #
+    #=====================================================================#
+    #---------------------------------------------------------------------#
+    # Preallocating arrays                                                #
+    #---------------------------------------------------------------------#
+    term1   = np.zeros((N+1, N+1, N+1))
+    term2   = np.zeros((N+1, N+1, N+1))
+    term3   = np.zeros((N+1, N+1, N+1))
+    term4   = np.zeros((N+1, N+1, N+1))
+    term5   = np.zeros((N+1, N+1, N+1))
+    term6   = np.zeros((N+1, N+1, N+1))
+    #---------------------------------------------------------------------#
+    # Calculating terms in the exact solution                             #
+    #---------------------------------------------------------------------#
+    print_count = 0
+    for k in range(0, N+1):
+        for j in range(0, N+1):
+            for i in range(0, N+1):
+                term1[i,j,k]    = 2.0*x[i]**3.0*y[j]*z[k]
+                term2[i,j,k]    = (x[i]*y[j])*(0.5*(x[i]**2.0*z[k]+1))
+                term3[i,j,k]    = (x[i]*z[k])*(0.5*(x[i]**2.0*y[j] + 3.0))
+                term4[i,j,k]    = y[j]**2.0
+                term5[i,j,k]    = (3.0/2.0)*(y[j]*z[k])
+                term6[i,j,k]    = z[k]**2.0
+        #-----------------------------------------------------------------#
+        # Print statement                                                 #
+        #-----------------------------------------------------------------#
+        if print_count > 5:
+            print(k)
+            print_count = 0
+        print_count += 1
+    #---------------------------------------------------------------------#
+    # Calculating the exact solution                                      #
+    #---------------------------------------------------------------------#
+    P_exact = term1 + 2.0*term2 + 2.0*term3 + term4 + 2.0*term5 + term6
+    #=====================================================================#
+    # Error                                                               #
+    #=====================================================================#
+    error   = abs(P_exact - P_approx)
+    #=====================================================================#
+    # Plotting solutions                                                  #
+    #=====================================================================#
+    #---------------------------------------------------------------------#
+    # Exact solution                                                      #
+    #---------------------------------------------------------------------#
+    cnt = plt.contourf(X1, X2, P_exact[:,:,int(N/2)], 500, cmap="jet")
     for c in cnt.collections:
         c.set_edgecolors("face")
-    plt.xlabel("0 \leq x_{1} \leq 2 \pi")
-    plt.ylabel("0 \leq x_{2} \leq 2 \pi")
+    plt.xlabel("$0 \leq x_{1} \leq 1$")
+    plt.ylabel("$0 \leq x_{2} \leq 1$")
     plt.colorbar()
-    plt.show()
+    plt.savefig(media_path + "P-term-exact.pdf")
+    plt.clf()
+    #---------------------------------------------------------------------#
+    # Approximate solution                                                #
+    #---------------------------------------------------------------------#
+    cnt = plt.contourf(X1, X2, P_approx[:,:,int(N/2)], 500, cmap="jet")
+    for c in cnt.collections:
+        c.set_edgecolors("face")
+    plt.xlabel("$0 \leq x_{1} \leq 1$")
+    plt.ylabel("$0 \leq x_{2} \leq 1$")
+    plt.colorbar()
+    plt.savefig(media_path + "P-term-approx.pdf")
+    plt.clf()
+    #---------------------------------------------------------------------#
+    # Error solution                                                      #
+    #---------------------------------------------------------------------#
+    cnt = plt.contourf(X1, X2, error[:,:,int(N/2)], 500, cmap="jet")
+    for c in cnt.collections:
+        c.set_edgecolors("face")
+    plt.xlabel("$0 \leq x_{1} \leq 1$")
+    plt.ylabel("$0 \leq x_{2} \leq 1$")
+    plt.colorbar()
+    plt.savefig(media_path + "P-term-error.pdf")
+    plt.clf()
 
     print("**** Successful Run ****")
     sys.exit(0)
