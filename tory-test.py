89286745d4a76ba374912a05ee19fc71d1d85696
Toribio Fierro <fierro@toribios-mbp.3521.dhcp.asu.edu>
2019-12-22 20:25:16 -0700

    test for tori can see how python and git work

---
+++ tory-test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+N   = 128
+pi  = np.pi
+x   = np.linspace(0, 2.0*pi, N)
+y   = np.sin(pi*x)
+
+plt.plot(x,y,'r', lw=1.5)
+plt.xlabel('$0 \leq x \leq 2$')
+plt.ylabel('$f(x)$')
+plt.show()
