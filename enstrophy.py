5e39fecf0e21dc7557dba0237b081828f3bc2eb3
emiliotorres17 <emiliotorres17@yahoo.com>
2020-01-12 14:44:01 -0700

    added subroutines to calculate the Q and R terms from Dahm's notes

--- enstrophy.py
+++ enstrophy.py
@@ -29,7 +29,7 @@ def enstrophy_field(
 
     """ Calculating the enstrophy field """
     #---------------------------------------------------------------------#
-    # Determining the shape of the KE field                               #
+    # Determining the shape of the enstrophy field                        #
     #---------------------------------------------------------------------#
     dim     = omega1.shape
     ens     = np.empty((dim[0], dim[1], dim[2], dim[3]))
@@ -51,6 +51,80 @@ def enstrophy_field(
         print_count += 1
 
     return ens
+#-------------------------------------------------------------------------#
+# Q term from Dahm's emailed notes                                        #
+#-------------------------------------------------------------------------#
+def Q_term(
+        omega1,             # vorticity-1 component
+        omega2,             # vorticity-2 component
+        omega3,             # vorticity-3 component
+        s11,                # strain rate-11 component
+        s12,                # strain rate-12 component
+        s13,                # strain rate-13 component
+        s22,                # strain rate-22 component
+        s23,                # strain rate-23 component
+        s33):               # strain rate-33 component
+
+    """ Calculating the Q term from Dahm's note """
+    #---------------------------------------------------------------------#
+    # Numerator and denominator                                           #
+    #---------------------------------------------------------------------#
+    num     = omega1*s11*omega1 + omega1*s12*omega2 +  omega1*s13*omega3 +\
+                omega2*s12*omega1 + omega2*s22*omega2 + omega2*s23*omega3+\
+                omega3*s13*omega1 + omega3*s23*omega2 + omega3*s33*omega3
+    den1    = omega1*omega1 + omega2*omega2 + omega3*omega3
+    den2    = (s11*s11 + s12*s12 + s13*s13 + s12*s12 + s22*s22 + s23*s23 +\
+                s13*s13 + s23*s23 + s33*s33)**0.5
+    den     = ((2.0/3.0)**0.5)* den1 * den2
+    #---------------------------------------------------------------------#
+    # Q calculation                                                       #
+    #---------------------------------------------------------------------#
+    Q       = num/den
+
+    return Q
+#-------------------------------------------------------------------------#
+# R term from Dahm's emailed notes                                        #
+#-------------------------------------------------------------------------#
+def R_term(
+        enst,               # enstrophy field
+        omega1,             # vorticity-1 component
+        omega2,             # vorticity-2 component
+        omega3,             # vorticity-3 component
+        s11,                # strain rate-11 component
+        s12,                # strain rate-12 component
+        s13,                # strain rate-13 component
+        s22,                # strain rate-22 component
+        s23,                # strain rate-23 component
+        s33):               # strain rate-33 component
+
+    """ Calculating the R term from Dahm's scanned notes
+            **** Note: need to update the gradient terms to use spectral
+            methods """
+    #---------------------------------------------------------------------#
+    # Defining domain variables                                           #
+    #---------------------------------------------------------------------#
+    pi      = np.pi                             # pi
+    dx      = (2.0*pi)/64.0                     # spatial step
+    nu      = 0.000185                          # default viscosity
+    #---------------------------------------------------------------------#
+    # Numerator (need to update to spectral method)                       #
+    #---------------------------------------------------------------------#
+    term1   = np.gradient(enst,dx, edge_order=2)[0]
+    term2   = np.gradient(enst,dx, edge_order=2)[1]
+    term3   = np.gradient(enst,dx, edge_order=2)[2]
+    num     = nu*(term1 + term2 + term3)
+    #---------------------------------------------------------------------#
+    # Denominator                                                         #
+    #---------------------------------------------------------------------#
+    den     = omega1*s11*omega1 + omega1*s12*omega2 +  omega1*s13*omega3 +\
+                omega2*s12*omega1 + omega2*s22*omega2 + omega2*s23*omega3+\
+                omega3*s13*omega1 + omega3*s23*omega2 + omega3*s33*omega3
+    #---------------------------------------------------------------------#
+    # R calculation                                                       #
+    #---------------------------------------------------------------------#
+    R       = num/den
+
+    return R
 #=========================================================================#
 # Main                                                                    #
 #=========================================================================#
