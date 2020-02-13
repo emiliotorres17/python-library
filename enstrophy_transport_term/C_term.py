62768d2b968b4b54953403acad977636c6fd5d3a
emiliotorres170 <emilio.torres@pnnl.gov>
2020-02-13 00:26:11 -0700

    added unit testing

--- enstrophy_transport_term/C_term.py
+++ enstrophy_transport_term/C_term.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """========================================================================
 Purpose:
     The purpose of this script to calculate the C term, laplacian(Omega),
@@ -17,16 +17,16 @@ import os
 import sys
 from subprocess import call
 import numpy as np
-import matplotlob.pyplot as plt
+import matplotlib.pyplot as plt
 #=========================================================================#
 # User defined functions                                                  #
 #=========================================================================#
 #-------------------------------------------------------------------------#
 # C term                                                                  #
 #-------------------------------------------------------------------------#
-def c_term(
+def c_term_enstrophy(
         enst,                           # enstrophy field
-        dx      = False,                # spatial step size
+        h       = False,                # spatial step size
         Nu      = False,                # viscosity
         flag    = True):                # spectral flag; default is gradient
 
@@ -34,23 +34,23 @@ def c_term(
     #---------------------------------------------------------------------#
     # Default variables                                                   #
     #---------------------------------------------------------------------#
-    if dx is False:
+    if h is False:
         num = 64
         Pi  = np.pi
-        dx  = (2.0*pi)/num
+        h  = (2.0*pi)/num
     if Nu is False:
         Nu  = 0.000185
     #---------------------------------------------------------------------#
     # Calculating the Laplacian                                           #
     #---------------------------------------------------------------------#
-    diim    = enst.shape[0]
+    dim = enst.shape[0]
     if flag is True:
-        term1   = np.gradient(np.gradient(enst, dx, edge_order=2)[0],\
-                                dx, edge_order=2)[0]
-        term2   = np.gradient(np.gradient(enst, dx, edge_order=2)[1],\
-                                dx, edge_order=2)[1]
-        term3   = np.gradient(np.gradient(enst, dx, edge_order=2)[2],\
-                                dx, edge_order=2)[2]
+        term1   = np.gradient(np.gradient(enst, h, edge_order=2)[0],\
+                                h, edge_order=2)[0]
+        term2   = np.gradient(np.gradient(enst, h, edge_order=2)[1],\
+                                h, edge_order=2)[1]
+        term3   = np.gradient(np.gradient(enst, h, edge_order=2)[2],\
+                                h, edge_order=2)[2]
     else:
         term1   = np.fft.ifftn(1j*Kfield[0]*np.fft.fftn(enst)).real
         term1   = np.fft.ifftn(1j*Kfield[0]*np.fft.fftn(term1)).real
@@ -73,6 +73,66 @@ if __name__ == "__main__":
     # Main preamble                                                       #
     #---------------------------------------------------------------------#
     call(["clear"])
-    print("**** Has not been unit tested ****")
+    sep         = os.sep
+    pwd         = os.getcwd()
+    media_path  = pwd + "%cmedia%c"         %(sep, sep)
+    #---------------------------------------------------------------------#
+    # Domain variables                                                    #
+    #---------------------------------------------------------------------#
+    pi          = np.pi
+    N           = 128
+    x0          = -1.0
+    xf          = 1.0
+    dx          = (xf-x0)/N
+    x           = np.linspace(x0, xf, N+1)
+    y           = np.linspace(x0, xf, N+1)
+    z           = np.linspace(x0, xf, N+1)
+    [X1, X2]    = np.meshgrid(x,y)
+    #---------------------------------------------------------------------#
+    # Preallocating space                                                 #
+    #---------------------------------------------------------------------#
+    enst        = np.zeros((N+1,N+1,N+1))
+    sol         = np.zeros((N+1,N+1,N+1))
+    #---------------------------------------------------------------------#
+    # Calculating approximate and exact solution                          #
+    #---------------------------------------------------------------------#
+    for k in range(0,N+1):
+        for j in range(0,N+1):
+            for i in range(0,N+1):
+                enst[i,j,k] = x[i]**2.0*y[j]**2.0*z[k]**2.0
+                sol[i,j,k]  = 2.0*y[j]**2.0*z[k]**2.0 +\
+                                2.0*x[i]**2.0*z[k]**2.0 +\
+                                2.0*x[i]**2.0*y[j]**2.0
+        print(k)
+    c_approx    = c_term_enstrophy(enst, dx, 1.0, True)
+    #---------------------------------------------------------------------#
+    # Plotting approximate solution                                       #
+    #---------------------------------------------------------------------#
+    cnt     = plt.contourf(X1, X2, c_approx[:,:,31], 500, cmap='jet')
+    for c in cnt.collections:
+        c.set_edgecolors("face")
+    plt.colorbar()
+    plt.savefig(media_path + "C-term-approx.pdf")
+    plt.clf()
+    #---------------------------------------------------------------------#
+    # Plotting exact solution                                             #
+    #---------------------------------------------------------------------#
+    cnt     = plt.contourf(X1, X2, sol[:,:,31], 500, cmap='jet')
+    for c in cnt.collections:
+        c.set_edgecolors("face")
+    plt.colorbar()
+    plt.savefig(media_path + "C-term-exact.pdf")
+    plt.clf()
+    #---------------------------------------------------------------------#
+    # Error                                                               #
+    #---------------------------------------------------------------------#
+    error   = abs(sol-c_approx)
+    cnt     = plt.contourf(X1, X2, error[:,:,31], 500, cmap='jet')
+    for c in cnt.collections:
+        c.set_edgecolors("face")
+    plt.colorbar()
+    plt.savefig(media_path + "C-term-error.pdf")
+    plt.clf()
 
+    print("**** Successful Run ****")
     sys.exit(0)
