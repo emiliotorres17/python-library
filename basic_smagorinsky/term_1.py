6953893a75c4ce0533f2b4bf6701e3fecc6a3e50
emiliotorres17 <emiliotorres17@yahoo.com>
2020-04-21 22:11:44 -0700

    changed the approach to calculate term1

--- basic_smagorinsky/term_1.py
+++ basic_smagorinsky/term_1.py
@@ -25,22 +25,51 @@ import numpy as np
 # Term 1                                                                  #
 #-------------------------------------------------------------------------#
 def term_1(
-        nu_sgs,         # strain rates
-        b,              # B term in the enstrophy transport equation
-        d,              # D term in the enstrophy transport equation
-        nu  = True):    # kinematic viscosity; default is set to 0.000185 
+        omega1,                 # vorticity-1
+        omega2,                 # vorticity-2
+        omega3,                 # vorticity-3
+        enst,                   # enstrophy
+        nu_sgs,                 # turbulent viscosity
+        h       = True):        # spatial step size
 
     """ Calculating term 1 in the SGS enstrophy study """
     #---------------------------------------------------------------------#
     # Setting default values                                              #
     #---------------------------------------------------------------------#
-    if nu is True:
-        nu = 0.000185
+    if h is True:
+        h = 2.0*np.pi/64.0
     #---------------------------------------------------------------------#
-    # Term 1                                                              #
+    # Preallocating space                                                 #
     #---------------------------------------------------------------------#
     term    = np.zeros((64,64,64))
-    term    += (nu_sgs/nu)*(b+d)
+    #---------------------------------------------------------------------#
+    # Enstrophy term                                                      #
+    #---------------------------------------------------------------------#
+    term    += np.gradient(\
+                np.gradient(enst, h, edge_order=2)[2], h, edge_order=2)[2]
+    term    += np.gradient(\
+                np.gradient(enst, h, edge_order=2)[1], h, edge_order=2)[1]
+    term    += np.gradient(\
+                np.gradient(enst, h, edge_order=2)[0], h, edge_order=2)[0]
+    #---------------------------------------------------------------------#
+    # Dissipation                                                         #
+    #---------------------------------------------------------------------#
+    omega1_grad = np.gradient(omega1, h, edge_order=2)
+    omega2_grad = np.gradient(omega2, h, edge_order=2)
+    omega3_grad = np.gradient(omega3, h, edge_order=2)
+    term        += np.square(omega1_grad[2])
+    term        += np.square(omega1_grad[1])
+    term        += np.square(omega1_grad[0])
+    term        += np.square(omega2_grad[2])
+    term        += np.square(omega2_grad[1])
+    term        += np.square(omega2_grad[0])
+    term        += np.square(omega3_grad[2])
+    term        += np.square(omega3_grad[1])
+    term        += np.square(omega3_grad[0])
+    #---------------------------------------------------------------------#
+    # Applying the subgrid stress                                         #
+    #---------------------------------------------------------------------#
+    term    *= nu_sgs
 
     return term
 #=========================================================================#
