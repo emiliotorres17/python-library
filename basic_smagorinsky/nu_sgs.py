936c1476365f10f37a29b6208ff431099882ba67
emilio <emiliotorres17@yahoo.com>
2020-05-06 00:25:03 -0700

    changed everything to closely natch what is in the ales code

--- basic_smagorinsky/nu_sgs.py
+++ basic_smagorinsky/nu_sgs.py
@@ -20,7 +20,7 @@ import numpy as np
 # User defined functions                                                  #
 #=========================================================================#
 def nu_sgs_BS(
-        S,
+        Sij,
         cs      = True,         # C_{S} value; default C_{S} = 0.17
         delta   = True):        # Delta value; default is 2*pi/30
 
@@ -36,17 +36,13 @@ def nu_sgs_BS(
     # Finding the magnitude of S_{ij}                                     #
     #---------------------------------------------------------------------#
     Smag    = np.zeros((64,64,64))
-    Smag    += np.square(S[0])              # S_{11}^2
-    Smag    += 2.0*np.square(S[1])          # S_{12}^2
-    Smag    += 2.0*np.square(S[2])          # S_{13}^2
-    Smag    += np.square(S[3])              # S_{22}^2
-    Smag    += 2.0*np.square(S[4])          # S_{23}^2
-    Smag    += np.square(S[5])              # S_{33}^3
-    Smag    = np.sqrt(Smag)                 # square root
+    Smag    += np.sqrt(2.0*(Sij[0]*Sij[0]+2.0*Sij[1]*Sij[1]+\
+                        2.0*Sij[2]*Sij[2]+ Sij[3]*Sij[3]+2.0*Sij[4]*Sij[4]\
+                        +Sij[5]*Sij[5]))
     #---------------------------------------------------------------------#
     # Calculating nu_{sgs}                                                #
-    #---------------------------------------------------------------------#
-    nu_sgs = 2.0*(cs*delta)**2.0*Smag
+    #--------------------------------------------------------------------#
+    nu_sgs = -2.0*(cs*delta)**2.0*Smag
 
     return nu_sgs
 #=========================================================================#
