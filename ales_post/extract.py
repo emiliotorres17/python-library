2e7a7456d66306b612bf3e755e0266df9eb31630
emiliotorres17 <emiliotorres17@yahoo.com>
2020-06-17 13:31:34 -0700

    added strain rates and sgs; both of them output in 1-array form

--- ales_post/extract.py
+++ ales_post/extract.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     A_path_enst     = pwd + "%c..%cA-enst%c"            %(sep, sep, sep)
     B_path_enst     = pwd + "%c..%cB-enst%c"            %(sep, sep, sep)
     D_path_enst     = pwd + "%c..%cD-enst%c"            %(sep, sep, sep)
-    Pi_path_enst    = pwd + "%c..%cPi-enst%c"           %(sep, sep, sep)
+    C_path_enst     = pwd + "%c..%cC-enst%c"            %(sep, sep, sep)
     P_path_enst     = pwd + "%c..%cP-enst%c"            %(sep, sep, sep)
     A_path_ke       = pwd + "%c..%cA-ke%c"              %(sep, sep, sep)
     B_path_ke       = pwd + "%c..%cB-ke%c"              %(sep, sep, sep)
@@ -57,6 +57,8 @@ if __name__ == "__main__":
     omega1_path     = pwd + "%c..%comega1%c"            %(sep, sep, sep)
     omega2_path     = pwd + "%c..%comega2%c"            %(sep, sep, sep)
     omega3_path     = pwd + "%c..%comega3%c"            %(sep, sep, sep)
+    S_path          = pwd + "%c..%cstrain-rates%c"      %(sep, sep, sep)
+    tau_path        = pwd + "%c..%ctau%c"               %(sep, sep, sep)
     #---------------------------------------------------------------------#
     # Domain variables                                                    #
     #---------------------------------------------------------------------#
@@ -104,9 +106,9 @@ if __name__ == "__main__":
         #-----------------------------------------------------------------#
         # Extracting Pi spectral                                          #
         #-----------------------------------------------------------------#
-        Pi   = npy_transport_term_general('Pi', 64, 16, tstart, tf, Pi_path_enst)
-        np.save(data_path + "Pi-enst.npy", Pi)
-        del Pi
+        C   = npy_transport_term_general('C', 64, 16, tstart, tf, C_path_enst)
+        np.save(data_path + "C-enst.npy", D)
+        del C
         #-----------------------------------------------------------------#
         # Extracting P spectral                                           #
         #-----------------------------------------------------------------#
@@ -211,45 +213,92 @@ if __name__ == "__main__":
     # Strain rates                                                        #
     #=====================================================================#
     if S_flag is True:
+        S   = np.zeros((6,64,64,64,tf+1))
         #-----------------------------------------------------------------#
         # S-11                                                            #
         #-----------------------------------------------------------------#
-        s11     = npy_S_general(11, 64, 16, 0 tf, S_path)
-        np.save(data_path + 's11.npy', s11)
+        s11     = npy_S_general(11, 64, 16, 0, tf, S_path)
+        S[0]    = s11
+        #np.save(data_path + 's11.npy', s11)
         del s11
         #-----------------------------------------------------------------#
         # S-12                                                            #
         #-----------------------------------------------------------------#
-        s12 = npy_S_general(12, 64, 16, 0 tf, S_path)
-        np.save(data_path + 's12.npy', s12)
+        s12     = npy_S_general(12, 64, 16, 0, tf, S_path)
+        S[1]    = s12
+        #np.save(data_path + 's12.npy', s12)
         del s12
         #-----------------------------------------------------------------#
         # S-13                                                            #
         #-----------------------------------------------------------------#
-        s13 = npy_S_general(13, 64, 16, 0 tf, S_path)
-        np.save(data_path + 's13.npy', s13)
+        s13     = npy_S_general(13, 64, 16, 0, tf, S_path)
+        S[2]    = s13
+        #np.save(data_path + 's13.npy', s13)
         del s13
         #-----------------------------------------------------------------#
         # S-22                                                            #
         #-----------------------------------------------------------------#
-        s22 = npy_S_general(22, 64, 16, 0 tf, S_path)
-        np.save(data_path + 's22.npy', s22)
+        s22     = npy_S_general(22, 64, 16, 0, tf, S_path)
+        S[3]    = s22
+        #np.save(data_path + 's22.npy', s22)
         del s22
         #-----------------------------------------------------------------#
         # S-23                                                            #
         #-----------------------------------------------------------------#
-        s23 = npy_S_general(23, 64, 16, 0 tf, S_path)
-        np.save(data_path + 's23.npy', s23)
+        s23     = npy_S_general(23, 64, 16, 0, tf, S_path)
+        S[4]    = s23
+        #np.save(data_path + 's23.npy', s23)
         del s23
         #-----------------------------------------------------------------#
         # S-33                                                            #
         #-----------------------------------------------------------------#
-        s33 = npy_S_general(33, 64, 16, 0 tf, S_path)
-        np.save(data_path + 's33.npy', s33)
+        s33 = npy_S_general(33, 64, 16, 0, tf, S_path)
+        S[5]    = s23
+        #np.save(data_path + 's33.npy', s33)
         del s33
-
-    print("**** Successful Run ****")
-    sys.exit(0)
+        np.save(data_path + 'strain-rates.npy', S)
+    #=====================================================================#
+    # Subgrid stress                                                      #
+    #=====================================================================#
+    if tau_flag is True:
+        tau   = np.zeros((6,64,64,64,tf+1))
+        #-----------------------------------------------------------------#
+        # tau-11                                                          #
+        #-----------------------------------------------------------------#
+        tau11     = npy_tau_general(11, 64, 16, 0, tf, S_path)
+        tau[0]    = tau11
+        del tau11
+        #-----------------------------------------------------------------#
+        # tau-12                                                          #
+        #-----------------------------------------------------------------#
+        tau12     = npy_tau_general(12, 64, 16, 0, tf, S_path)
+        tau[1]    = tau12
+        del tau12
+        #-----------------------------------------------------------------#
+        # tau-13                                                          #
+        #-----------------------------------------------------------------#
+        tau13     = npy_tau_general(13, 64, 16, 0, tf, S_path)
+        tau[2]    = tau13
+        del tau13
+        #-----------------------------------------------------------------#
+        # tau-22                                                          #
+        #-----------------------------------------------------------------#
+        tau22     = npy_tau_general(22, 64, 16, 0, tf, S_path)
+        tau[3]    = tau22
+        del tau22
+        #-----------------------------------------------------------------#
+        # tau-23                                                          #
+        #-----------------------------------------------------------------#
+        tau23     = npy_tau_general(23, 64, 16, 0, tf, S_path)
+        tau[4]    = tau23
+        del tau23
+        #-----------------------------------------------------------------#
+        # tau-33                                                          #
+        #-----------------------------------------------------------------#
+        tau33   = npy_tau_general(33, 64, 16, 0, tf, S_path)
+        tau[5]  = tau23
+        del tau33
+        np.save(data_path + 'tau.npy', tau)
 
     print("**** Successful Run ****")
     sys.exit(0)
