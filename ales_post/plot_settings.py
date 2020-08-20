1144d2e7af856ff976b25a77d04107c34f09a6d4
emiliotorres17 <emiliotorres17@yahoo.com>
2020-08-19 22:18:58 -0700

    made it a subroutine

--- ales_post/plot_settings.py
+++ ales_post/plot_settings.py
@@ -1,15 +1,23 @@
+#!/usr/bin/env python3
+#---------------------------------------------------------------------#
+# Python packages                                                     #
+#---------------------------------------------------------------------#
+import matplotlib.pyplot as plt
 #---------------------------------------------------------------------#
 # Font settings                                                       #
 #---------------------------------------------------------------------#
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-SMALL_SIZE = 14
-MEDIUM_SIZE = 18
-BIGGER_SIZE = 12
-plt.rc('font',      size=SMALL_SIZE)            # controls default text sizes
-plt.rc('axes',      titlesize=SMALL_SIZE)       # fontsize of the axes title
-plt.rc('axes',      labelsize=MEDIUM_SIZE)      # fontsize of the x and y labels
-plt.rc('xtick',     labelsize=SMALL_SIZE)       # fontsize of the tick labels
-plt.rc('ytick',     labelsize=SMALL_SIZE)       # fontsize of the tick labels
-plt.rc('legend',    fontsize=SMALL_SIZE)        # legend fontsize
-plt.rc('figure',    titlesize=BIGGER_SIZE)      # fontsize of the figure title
+def plot_setting():
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    SMALL_SIZE = 14
+    MEDIUM_SIZE = 18
+    BIGGER_SIZE = 12
+    plt.rc('font',      size=SMALL_SIZE)            # controls default text sizes
+    plt.rc('axes',      titlesize=SMALL_SIZE)       # fontsize of the axes title
+    plt.rc('axes',      labelsize=MEDIUM_SIZE)      # fontsize of the x and y labels
+    plt.rc('xtick',     labelsize=SMALL_SIZE)       # fontsize of the tick labels
+    plt.rc('ytick',     labelsize=SMALL_SIZE)       # fontsize of the tick labels
+    plt.rc('legend',    fontsize=SMALL_SIZE)        # legend fontsize
+    plt.rc('figure',    titlesize=BIGGER_SIZE)      # fontsize of the figure title
+    
+    return
