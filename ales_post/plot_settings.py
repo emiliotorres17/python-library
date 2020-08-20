05a021ad236d5db99a6faff274c90fcf4247752c
emilio <emiliotorres17@yahoo.com>
2020-08-20 00:46:50 -0700

    added x and y tick mark functionality

--- ales_post/plot_settings.py
+++ ales_post/plot_settings.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
-#---------------------------------------------------------------------#
-# Python packages                                                     #
-#---------------------------------------------------------------------#
+#-------------------------------------------------------------------------#
+# Python packages                                                         #
+#-------------------------------------------------------------------------#
 import matplotlib.pyplot as plt
-#---------------------------------------------------------------------#
-# Font settings                                                       #
-#---------------------------------------------------------------------#
-def plot_setting():
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter,\
+                                AutoMinorLocator
+#-------------------------------------------------------------------------#
+# Font settings                                                           #
+#-------------------------------------------------------------------------#
+def plot_setting(
+        major_tick  = False,
+        minor_tick  = False,
+        tick_format = False,
+        y_tick      = False):
+    #---------------------------------------------------------------------#
+    # Font sizes                                                          #
+    #---------------------------------------------------------------------#
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
     SMALL_SIZE = 14
@@ -19,5 +28,16 @@ def plot_setting():
     plt.rc('ytick',     labelsize=SMALL_SIZE)       # fontsize of the tick labels
     plt.rc('legend',    fontsize=SMALL_SIZE)        # legend fontsize
     plt.rc('figure',    titlesize=BIGGER_SIZE)      # fontsize of the figure title
-    
-    return
+    #---------------------------------------------------------------------#
+    # Setting tick marks                                                  #
+    #---------------------------------------------------------------------#
+    fig, ax = plt.subplots()
+    if major_tick is not False:
+        ax.xaxis.set_major_locator(MultipleLocator(major_tick))
+        ax.xaxis.set_major_formatter(FormatStrFormatter(tick_format))
+        ax.xaxis.set_minor_locator(MultipleLocator(minor_tick))
+    if y_tick is not False:
+        ax.yaxis.set_major_locator(MultipleLocator(major_tick))
+        ax.yaxis.set_major_formatter(FormatStrFormatter(tick_format))
+        ax.yaxis.set_minor_locator(MultipleLocator(minor_tick))
+    return fig, ax 
