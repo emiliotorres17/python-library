b2cb5042def3936c1c6a7de0c0bbaacbb77fa8fb
emilio <emiliotorres17@yahoo.com>
2021-01-29 20:03:58 -0700

    added a tool to visualize the locations

--- ales_post/back_tracking_subroutines/back-tracking.py
+++ ales_post/back_tracking_subroutines/back-tracking.py
@@ -15,7 +15,7 @@ Author:
 import os
 import sys
 from subprocess import call
-from numpy import *
+from numpy import zeros, load, loadtxt, linspace, pi, flipud
 import matplotlib.pyplot as plt
 #-------------------------------------------------------------------------#
 # User packages                                                           #
@@ -24,6 +24,71 @@ from ales_post.plot_settings    import plot_setting
 from min_max                    import find_max3D
 from rk4_back_tracking          import rk4_back_tracking
 from rk4_back_tracking          import ghost_cells
+#=========================================================================#
+# User defined functions                                                  #
+#=========================================================================#
+#-------------------------------------------------------------------------#
+# Tracking locations                                                      #
+#-------------------------------------------------------------------------#
+def plot_x_location(
+        Locs,
+        T_steps,
+        T_vals,
+        direction,
+        name        = False,
+        time_flag   = False):
+
+    """ Plotting different x locations """
+
+    #---------------------------------------------------------------------#
+    # Default is time steps                                               #
+    #---------------------------------------------------------------------#
+    if time_flag is True:
+        x_vec   = T_vals
+        xlab    = 'Time'
+    else:
+        x_vec   = T_steps
+        xlab    = 'Time step'
+    #---------------------------------------------------------------------#
+    # Plotting location                                                   #
+    #---------------------------------------------------------------------#
+    plot_setting()
+    syms    = ['r', 'b', 'g', 'k', 'm', 'c']
+    for q, T in enumerate(test):
+        plt.plot(x_vec[q], Locs[q], syms[q], \
+                    label='$t_{0} + %i\\Delta t$'   %(int(T-tzero)))
+        #-----------------------------------------------------------------#
+        # Plotting the location of BS and DS switch                       #
+        #-----------------------------------------------------------------#
+        plt.plot([tzero, tzero], [7,-7], 'b--', lw=1.5)
+    #---------------------------------------------------------------------#
+    # Plot settings                                                       #
+    #---------------------------------------------------------------------#
+    plt.grid(True)
+    plt.legend(loc=0)
+    plt.ylabel('$x_{%i}$ location'          %(int(direction)))
+    plt.xlabel(xlab)
+    plt.ylim([0., 2.0*pi])
+    plt.yticks([0, 0.5*pi, pi, 1.5*pi, 2.0*pi], \
+                [0,\
+                '$\\pi/2$',\
+                '$\\pi$',\
+                '$3\\pi/2$',\
+                '$2\\pi$'])
+    #---------------------------------------------------------------------#
+    # Flip axis                                                           #
+    #---------------------------------------------------------------------#
+    ax1 = plt.gca()
+    ax1.invert_xaxis()
+    #---------------------------------------------------------------------#
+    # Saving                                                              #
+    #---------------------------------------------------------------------#
+    if name is not False:
+        plt.layout_tight()
+        plt.savefig(media_path + name + '.png', bbox_inches='tight')
+    plt.show()
+
+    return
 #=========================================================================#
 # Main                                                                    #
 #=========================================================================#
@@ -56,14 +121,15 @@ if __name__ == '__main__':
     # Loading enstrophy                                                   #
     #---------------------------------------------------------------------#
     print('Loading data:')
-    ts          = 0 
-    tf          = 251 
-    tint        = 240
+    test        = [285, 280, 275]
+    ts          = 0
+    tf          = 289
+    tzero       = 215
     time        = load(data_path + 'time.npy')[ts:tf]
     dt          = zeros(len(time)-1)
     count       = zeros(len(time)-1)
     print('\ttime')
-    enst        = load(data_path + 'enst.npy')[:,:,:,ts:tf]    
+    enst        = load(data_path + 'enst.npy')[:,:,:,ts:tf]
     print(enst.shape)
     print('\tenstrophy')
 
@@ -73,53 +139,89 @@ if __name__ == '__main__':
     print(time[-1])
     print(mean(enst[:,:,:,-1]))
     #---------------------------------------------------------------------#
-    # Maximum values and coordinates                                      #
+    # Initializing strings                                                #
     #---------------------------------------------------------------------#
-    [val, xc, yc, zc]   = find_max3D(enst[:,:,:,tint])
-    print('enst max --> %10.5e'                 %(val))
-    print('x-index = %3i\tx-value = %8.4e'      %(xc, x[xc]))
-    print('y-index = %3i\ty-value = %8.4e'      %(yc, y[yc]))
-    print('z-index = %3i\tz-value = %8.4e'      %(zc, z[zc]))
+    x1_loc  = []
+    x2_loc  = []
+    x3_loc  = []
+    t_val   = []
+    t_steps = []
     #---------------------------------------------------------------------#
-    # Initial point                                                       #
+    # Test loop                                                           #
     #---------------------------------------------------------------------#
-    x0  = 0.6652053603967715
-    y0  = 5.0357656588257953
-    z0  = 4.6776113491502915
-    t0  = 15.3050502065754515
+    IC      = ''
+    for count,  tint in enumerate(test):
+        #-----------------------------------------------------------------#
+        # Maximum values and coordinates                                  #
+        #-----------------------------------------------------------------#
+        [val, xc, yc, zc]   = find_max3D(enst[:,:,:,tint])
+        print('enst max --> %10.5e'                 %(val))
+        print('x-index = %3i\tx-value = %8.4e'      %(xc, x[xc]))
+        print('y-index = %3i\ty-value = %8.4e'      %(yc, y[yc]))
+        print('z-index = %3i\tz-value = %8.4e'      %(zc, z[zc]))
+        #-----------------------------------------------------------------#
+        # Initial point                                                   #
+        #-----------------------------------------------------------------#
+        x0  = x[xc]
+        y0  = x[yc]
+        z0  = x[zc]
+        t0  = time[tint]
+        IC  += '\ttval=%i\tval=%5.3e\tx0=%5.3f\tx1=%5.3f\tx1=%5.3f\n\n'\
+                %(tint, val, x0, y0, z0)
+        #-----------------------------------------------------------------#
+        # Domain variables                                                #
+        #-----------------------------------------------------------------#
+        dx  = 2.*pi/N
+        x1  = linspace(-0.5*dx, 2.0*pi+0.5*dx, N+2)
+        x2  = linspace(-0.5*dx, 2.0*pi+0.5*dx, N+2)
+        x3  = linspace(-0.5*dx, 2.0*pi+0.5*dx, N+2)
+        #-----------------------------------------------------------------#
+        # Loading velocity                                                #
+        #-----------------------------------------------------------------#
+        print('Loading data:')
+        u1          = load(data_path + 'velocity1.npy')[:,:,:,ts:tf]
+        u1          = ghost_cells(N, u1)
+        print('\tvelocity-1')
+        u2          = load(data_path + 'velocity2.npy')[:,:,:,ts:tf]
+        u2          = ghost_cells(N, u2)
+        print('\tvelocity-2')
+        u3          = load(data_path + 'velocity3.npy')[:,:,:,ts:tf]
+        u3          = ghost_cells(N, u3)
+        print('\tvelocity-3')
+        #-----------------------------------------------------------------#
+        # Initial start point                                             #
+        #-----------------------------------------------------------------#
+        print('%25.20f'     %(t0))
+        print('%25.20f'     %(x0))
+        print('%25.20f'     %(y0))
+        print('%25.20f'     %(z0))
+        #-----------------------------------------------------------------#
+        # Back tracking                                                   #
+        #-----------------------------------------------------------------#
+        rk4_back_tracking([x0, y0, z0, t0], u1, u2, u3, \
+                            x1, x2, x3, time, tint, 0,\
+                            'enstrophy-back-print.out', \
+                            'enstrophy-back-coordinates.out')
+        #-----------------------------------------------------------------#
+        # Storing x1-x3                                                   #
+        #-----------------------------------------------------------------#
+        data    = loadtxt('enstrophy-back-coordinates.out',skiprows=1)
+        x1_loc.append(data[:,1])
+        x2_loc.append(data[:,2])
+        x3_loc.append(data[:,3])
+        t_val.append(data[:,0])
+        t_steps.append(flipud(linspace(0, tint, len(x1_loc[count]))))
     #---------------------------------------------------------------------#
-    # Domain variables                                                    #
+    # Plotting data                                                       #
     #---------------------------------------------------------------------#
-    dx          = 2.*pi/N
-    x1          = linspace(-0.5*dx, 2.0*pi+0.5*dx, N+2)
-    x2          = linspace(-0.5*dx, 2.0*pi+0.5*dx, N+2)
-    x3          = linspace(-0.5*dx, 2.0*pi+0.5*dx, N+2)
+    plot_x_location( x1_loc, t_steps, t_val, 1)
+    plot_x_location( x2_loc, t_steps, t_val, 2)
+    plot_x_location( x3_loc, t_steps, t_val, 3)
     #---------------------------------------------------------------------#
-    # Loading velocity                                                    #
+    # Printing initial conditions                                         #
     #---------------------------------------------------------------------#
-    print('Loading data:')
-    u1          = load(data_path + 'velocity1.npy')[:,:,:,ts:tf]
-    u1          = ghost_cells(N, u1)
-    print('\tvelocity-1')
-    u2          = load(data_path + 'velocity2.npy')[:,:,:,ts:tf]
-    u2          = ghost_cells(N, u2)
-    print('\tvelocity-2')
-    u3          = load(data_path + 'velocity3.npy')[:,:,:,ts:tf]
-    u3          = ghost_cells(N, u3)
-    print('\tvelocity-3')
-    #---------------------------------------------------------------------#
-    # Initial start point                                                 #
-    #---------------------------------------------------------------------#
-    print('%25.20f'     %(t0))
-    print('%25.20f'     %(x0))
-    print('%25.20f'     %(y0))
-    print('%25.20f'     %(z0))
-    #---------------------------------------------------------------------#
-    # Back tracking                                                       #
-    #---------------------------------------------------------------------#
-    rk4_back_tracking([x0, y0, z0, t0], u1, u2, u3, \
-                        x1, x2, x3, time, tint, 0,\
-                        'enstrophy-back-print.out', 'enstrophy-back-coordinates.out')
-    call(['python3', 'plot-coordinates.py'])
+    call(['clear'])
+    print(IC)
+
     print('**** Successful run ****')
     sys.exit(0)
